9e284863e5faba49085044a1082b7fc79a99407c
Pablo Seoane <pablitoseoane@gmail.com>
2022-06-21 18:36:02 -0300

    cambios

--- famila/models.py
+++ famila/models.py
@@ -5,16 +5,21 @@ class Familia(models.Model):
     nombre = models.CharField(max_length=40)
     edad = models.IntegerField()
     Fecha_Nac = models.DateField()
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Edad: {self.edad} - Fecha Nacimiento: {self.Fecha_Nac}"
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
-    
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Camada: {self.camada}"
 
 class Alumno(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
     Fecha_Nac = models.DateField()
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Camada: {self.camada} - Fecha Nacimiento: {self.Fecha_Nac}"
     
 
 class Profesor(models.Model):
@@ -22,3 +27,5 @@ class Profesor(models.Model):
     Apellido = models.CharField(max_length=40)
     Especialidad = models.CharField(max_length=40)
     mail = models.EmailField()
+    def __str__(self):
+        return f"Nombre: {self.nombre} - Apellido: {self.Apellido} - Especialidad: {self.Especialidad} - Mail: {self.mail}"
